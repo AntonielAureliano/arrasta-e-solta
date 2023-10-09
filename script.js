7c557dafa084c2b3e1a6145ab01268804705aa26
Antoniel Aureliano <tonielaureliano@gmail.com>
2023-10-09 19:59:39 -0300

    Criando funções para o item ficar no local esperado.

---
+++ script.js
@@ -0,0 +1,34 @@
+document.querySelectorAll(".item").forEach((item) => {
+  item.addEventListener("dragstart", dragStart);
+  item.addEventListener("dragend", dragEnd);
+});
+
+document.querySelectorAll(".area").forEach((area) => {
+  area.addEventListener("dragover", dragOver);
+  area.addEventListener("dragleave", dragLeave);
+  area.addEventListener("drop", drop);
+});
+
+function dragStart(e) {
+  e.currentTarget.classList.add("dragging");
+}
+
+function dragEnd(e) {
+  e.currentTarget.classList.remove("dragging");
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  e.currentTarget.classList.add("hover");
+}
+function dragLeave(e) {
+  e.currentTarget.classList.remove("hover");
+}
+function drop(e) {
+  e.currentTarget.classList.remove("hover");
+
+  let dragItem = document.querySelector(".item.dragging");
+  if (e.currentTarget.querySelector(".item") === null) {
+    e.currentTarget.appendChild(dragItem);
+  }
+}
